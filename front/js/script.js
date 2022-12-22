443f40f1cedc2e38428e76cbdd0e6e9ee0baf7bf
dana971 <nac.dara@gmail.com>
2022-12-22 14:16:57 +0100

    Fetch  asynchrone
    Fetch  synchrone  to Fetch Asychrone  Etapes 1-3

--- front/js/script.js
+++ front/js/script.js
@@ -1,23 +1,27 @@
- fetch (" http://localhost:3000/api/products")
-    .then(function(res){
-        if(res.ok){
-            return res.json();
-        }
-    })
-    .then(function (value){
+const ENDPOINT = "http://localhost:3000/api/products"
+ // function to fetch all Kanaps
+/* getKanaps renvoi le resultat du fetch au format json -res.json()- */
+ const getKanaps = async (endpoint = ENDPOINT) => {
+    return await (await fetch(endpoint)).json();
+ }
 
-        for (let kanap of value) {
-            console.log(kanap.name);
-            document.querySelector("#items").innerHTML += `<a href="./product.html?id=${kanap._id}">
+ // Function to display Kanaps
+ const displayKanaps = (kanaps) => {
+    kanaps.forEach(kanap => {
+        document.querySelector("#items").innerHTML += `<a href="./product.html?id=${kanap._id}">
                 <article>
                     <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
                         <h3 class="productName">${kanap.name}</h3>
                         <p class="productDescription">${kanap.description}</p>
                 </article>
             </a>`;
-        }
-    });
-
+    })
+ }
+// Execute Script Functions
+ (async () => {
+     displayKanaps(await getKanaps())
+  // displayKanaps(await res.jon())
+ })()
 
 
 
