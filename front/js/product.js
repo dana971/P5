05c8e9dbeddd6e8472539cd79dc0c4b47b6f7e04
dana971 <nac.dara@gmail.com>
2022-12-22 14:12:56 +0100

    Page JS product Etape 7
    Affichage page produit + Ajout produits panier

--- front/js/product.js
+++ front/js/product.js
@@ -26,12 +26,88 @@ fetch (" http://localhost:3000/api/products/"+ id)
         let eltColors =  document.querySelector("select#colors");
         for (let i in kanap.colors ){
             // Creation d'un Element options qui lui meme engendre des elements options avec AppenChild
-           let opt = document.createElement("option");
-           eltColors.appendChild(opt)
-               opt.textContent = `${kanap.colors[i]}`;
+            let opt = document.createElement("option");
+            eltColors.appendChild(opt)
+            opt.textContent = `${kanap.colors[i]}`;
         }
 
     });
+
+let cartElement = document.querySelector("#addToCart");
+cartElement.addEventListener("click",function () {
+        // panier existe ou pas
+        let cartStorage = localStorage.getItem('storageCart');
+        let cartJson = JSON.parse(cartStorage);
+        let product;
+
+        // panier n'existe pas
+        if (cartStorage === null) {
+
+            // crééer le panier et ajoute le produit au panier
+            product = {
+                id: id,
+                color: getColor(),
+                qty: getQty(),
+            }
+            addProductToCart(product);
+
+        } else {
+            // panier exist
+            // le product qu'on ajoute est nouveau ou pas
+            product = {
+                id : id,
+                color: getColor(),
+                qty: getQty(),
+            }
+
+            // ont incremente sa qté
+            let foundProduct = cartJson.filter(p => {
+                return p.id === product.id && p.color === product.color;
+            } );
+           console.log(foundProduct);
+
+            if (foundProduct.length !== 0 ){ // produit existe
+                // find product in carjson
+                const aproduct = cartJson.find(p => {
+                    return p.id === product.id && p.color === product.color;
+                })
+                aproduct.qty = Number(product.qty) + Number(foundProduct[0].qty);
+                window.localStorage.setItem("storageCart", JSON.stringify(cartJson));
+            } else {
+                cartJson.push(product)
+                window.localStorage.setItem("storageCart", JSON.stringify(cartJson));
+            }
+        }
+    } // fin callback Event listener panier
+
+
+);
+
+// DISPLAY KANAP
+
+
+
+// get Color
+const getColor = () => {
+    let colorElement = document.getElementById("colors");
+    // let colorTab = colorElement.options[colorElement.selectedIndex].text;
+    return colorElement.value;
+}
+// get qty
+const getQty = () => {
+    let qtyElement = document.getElementById("quantity");
+    return Number(qtyElement.value);
+}
+
+// addtocart fxn
+function addProductToCart(product){
+    let cart = [];
+    cart.push(product);
+    // id et color = unique
+    window.localStorage.setItem("storageCart", JSON.stringify(cart));
+}
+
+// Get Cart data
 
 
 
